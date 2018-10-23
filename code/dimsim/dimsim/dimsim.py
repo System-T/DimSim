324a0ef5f710cd8cf0dc5cfd24c4f030d72ab092
qian.kun <qian.kun@ibm.com>
2018-10-23 08:48:17 -0700

    fix bug: missing vowel uo

--- code/dimsim/dimsim/dimsim.py
+++ code/dimsim/dimsim/dimsim.py
@@ -46,8 +46,8 @@ def to_pinyin(utterance):
 #         print("{} translates to {}".format(currPinyin, putToneToEnd(currPinyin)))
         translated.append(putToneToEnd(currPinyin))
     return translated
-       
-        
+
+
 def putToneToEnd(input_pinyin):
     if len(input_pinyin) is 1:
         return input_pinyin + '1'
@@ -73,16 +73,16 @@ def get_distance(utterance1, utterance2):
     else:
         u1 = to_pinyin(utterance1)
         u2 = to_pinyin(utterance2)
-        
+
         la = []
         lb = []
         for py in u1:
             la.append(Pinyin(py))
         for py in u2:
             lb.append(Pinyin(py))
-    
+
         res = 0.0
-        numDiff = 0        
+        numDiff = 0
         tot = len(utterance1)*2.1
         for i in range (len(utterance1)):
             apy = la[i]
@@ -91,43 +91,43 @@ def get_distance(utterance1, utterance2):
             if (apy is None) or (bpy is None):
                 print("Error {},{}".format(la, lb))
             res += getEditDistanceClose_TwoDCode(apy, bpy)
-            
+
             if apy.consonant is not bpy.consonant:
                 numDiff+=1
-            
+
             if not(str(apy.vowel) == str(bpy.vowel)):
                 numDiff+=1
-            
+
             if apy.tone is not bpy.tone:
                 numDiff+=0.01;
-                
+
         diffRatio = (numDiff)/tot;
         a = 0
         if diffRatio is 0:
             a=1
         return res*diffRatio;
-            
+
 def getEditDistanceClose_TwoDCode(a, b):
     res = 0
     try:
         if (a is None) or (b is None):
             print("Error:pinyin({},{})".format(a.toString(),b.toString()))
             return res
-        
+
         twoDcode_consonant_a = consonantMap_TwoDCode[a.consonant]
         twoDcode_consonant_b = consonantMap_TwoDCode[b.consonant]
-        
+
         cDis = abs(getDistance_TwoDCode(twoDcode_consonant_a, twoDcode_consonant_b))
-        
+
         twoDcode_vowel_a = vowelMap_TwoDCode[a.vowel]
         twoDcode_vowel_b = vowelMap_TwoDCode[b.vowel]
-        
+
         vDis = abs(getDistance_TwoDCode(twoDcode_vowel_a, twoDcode_vowel_b))
 
         hcDis = getSimDisFromHardcodMap(a,b)
-        
+
         res = min((cDis+vDis),hcDis) + 1.0*abs(a.tone-b.tone)/10
-        
+
     except:
         print("Error pinyin {}{}".format(a.toString(), b.toString()))
         raise
@@ -146,15 +146,15 @@ def getSimDisFromHardcodMap(a, b):
         return sys.float_info.max
     except:
         return sys.float_info.max
-    
-    
+
+
 def getDistance_TwoDCode(X, Y):
     x1, x2 = X
     y1, y2 = Y
 
     x1d = abs(x1-y1)
     x2d = abs(x2-y2)
-    
+
     return math.sqrt( x1d**2 + x2d**2)
 
 # def getEditDistanceClose(a, b):
@@ -162,7 +162,7 @@ def getDistance_TwoDCode(X, Y):
 #     try:
 #         if a is None or b is None:
 #             print("Error:pinyin({},{})".format(a.toString(),b.toString()))
-        
+
 #         res = abs(consonantMap[a.consonant] - consonantMap[b.consonant]) + abs(vowelMap[a.vowel] - vowelMap[b.vowel])
 #     except:
 #         print("Error pinyin {}{}".format(a.toString(), b.toString()))
@@ -175,40 +175,40 @@ def getDistance_TwoDCode(X, Y):
 
 consonantMap_TwoDCode ={
     "b":(1.0,0.5),
-    "p":(1.0,1.5), 
+    "p":(1.0,1.5),
 
-    "g":(7.0,0.5), 
-    "k":(7.0,1.5), 
-    "h":(7.0,3.0), 
-    "f":(7.0,4.0), 
+    "g":(7.0,0.5),
+    "k":(7.0,1.5),
+    "h":(7.0,3.0),
+    "f":(7.0,4.0),
 
-    "d":(12.0,0.5), 
-    "t":(12.0,1.5), 
+    "d":(12.0,0.5),
+    "t":(12.0,1.5),
 
-    "n":(22.5,0.5), 
-    "l":(22.5,1.5), 
-    "r":( 22.5,2.5), 
+    "n":(22.5,0.5),
+    "l":(22.5,1.5),
+    "r":( 22.5,2.5),
 
-    
-    "zh":(30,1.7), 
-    "z":(30,1.5), 
-    "j":(30.0,0.5), 
 
-    "ch":(31,1.7), 
-    "c":(31,1.5), 
-    "q":(31.0,0.5), 
+    "zh":(30,1.7),
+    "z":(30,1.5),
+    "j":(30.0,0.5),
+
+    "ch":(31,1.7),
+    "c":(31,1.5),
+    "q":(31.0,0.5),
 
     "sh":(33,3.7),
     "s":(33,3.5),
     "x":(33,2.5),
 
-    
-    "m":(50.0,3.5), 
 
-    "y":(40.0,0.0), 
+    "m":(50.0,3.5),
+
+    "y":(40.0,0.0),
     "w":(40,5.0),
-    
-    "":99999.0    
+
+    "":99999.0
 }
 
 
@@ -220,7 +220,7 @@ vowelMap_TwoDCode = {
     "an":(1.0,1.0),
     "ang":(1.0,1.5),
 
-    
+
     "ia":(0.0,0.0),
     "ian":(0.0,1.0),
     "iang":(0.0,1.5),
@@ -230,14 +230,14 @@ vowelMap_TwoDCode = {
     "uang":(2.0,1.5),
     "u:an":(2.0,1.0),
 
-    
+
     "ao":(5.0,0.0),
     "iao":(5.0,1.5),
 
     "ai":(8.0,0.0),
     "uai":(8.0,1.5),
 
-    
+
 
     "o":(20,0.0),
     "io":(20,2.5),
@@ -249,7 +249,7 @@ vowelMap_TwoDCode = {
     "ong":(20,8.0),
     "iong":(20,9.5),
 
-    
+
     "er":(41,1),
     "e":(41,0.0),
 
@@ -268,7 +268,7 @@ vowelMap_TwoDCode = {
     "un":(43,0.5),
     "ueng":(43,1.0),
 
-    
+
     "i":(60,1.0),
     "in":(60,2.5),
     "ing":(60,3.0),
@@ -280,7 +280,7 @@ vowelMap_TwoDCode = {
 
     "u":(80,0.0),
 
-    "":99999.0 
+    "":99999.0
 }
 
 
@@ -302,13 +302,13 @@ vowelList = ["a", "o", "e", "i", "u", "v","u:","er", "ao","ai", "ou","ei", "ia",
 class Pinyin:
     consonantList = ["b", "p", "m", "f", "d", "t", "n", "l", "g", "k", "h", "j", "q", "x", "zh", "ch", "sh", "r", "z", "c", "s","y", "w"]
     vowelList = ["a", "o", "e", "i", "u", "v","u:","er", "ao","ai", "ou","ei", "ia", "iao", "iu", "iou","ie", "ui","uei","ua","uo","uai", "u:e","ve",  "an", "en", "in", "un","uen", "vn","u:n","ian","uan", "u:an","van", "ang", "eng", "ing", "ong","iang","iong","uang","ueng"]
-    
+
     def __init__(self, pinyinstr):
         self.tone = int(pinyinstr[-1])
         self.locp = pinyinstr[0:-1].lower()
         self.consonant, self.vowel = self.parseConsonant(self.locp)
         self.pinyinRewrite()
-    
+
     def parseConsonant(self, pinyin):
         for consonant in consonantList:
             if pinyin.startswith(consonant):
@@ -316,25 +316,25 @@ class Pinyin:
         # it's a vowel without consonant
         if pinyin in vowelList:
             return None, pinyin.lower()
-        
+
         print("Invalid Pinyin, please check!")
         return None, None
-        
+
     def toStringNoTone(self):
         return "{}{}".format(self.consonant, self.vowel)
-    
+
     def toStringWithTone(self):
         return "{}{}{}".format(self.consonant, self.vowel, self.tone)
-    
+
     def toString(self):
         print("{}{}{}".format(self.consonant, self.vowel, self.tone))
-        
+
     def pinyinRewrite(self):
         yVowels = {"u","ue","uan","un","u:","u:e","u:an","u:n"}
         tconsonant = {"j","g","x"}
         if 'v' in self.vowel:
             self.vowel = self.vowel.replace("v", "u:")
-            
+
         if self.consonant is None or self.consonant is "":
             return
         if self.consonant is "y":
@@ -345,27 +345,27 @@ class Pinyin:
                 self.vowel="i"+self.vowel
                 self.vowel = self.vowel.replace("i+","i")
             self.consonant=""
-        
+
         if self.consonant is "w":
             self.vowel="u"+self.vowel;
             self.vowel=self.vowel.replace("u+","u")
             self.consonant = ""
-        
+
         if (self.consonant in tconsonant) and (self.vowel is "u") or (self.vowel is "v"):
             self.vowel="u:"
-        
+
         if self.vowel is "iou":
             self.vowel = "iu"
-        
+
         if self.vowel is "uei":
             self.vowel = "ui"
-        
+
         if self.vowel is "uen":
             self.vowel = "un"
-        
-                
-        
-        
+
+
+
+
 
 
 # In[666]:
@@ -391,37 +391,37 @@ hardcodeMap = {
 consonantMap = {
     "b":1.0,
     "p":2.0,
-    
+
     "m":11.0,
     "f":12.0,
-    
+
     "d":21.0,
     "t":22.0,
-    
+
     "n":31.0,
     "l":31.0,
     "r":32.0,
-    
+
     "g":41.0,
     "k":42.0,
     "h":43.0,
-    
+
     "j":46.0,
     "q":47.0,
     "x":48.0,
-    
+
     "z":61.0,
     "c":62.0,
-    
+
     "zh":71.0,
     "ch":72.0,
-    
+
     "sh":81.0,
     "s":82.0,
-    
+
     "y":90.0,
     "w":100.0,
-    
+
     "":99999.0,
     "__v":99999.0
 }
@@ -437,7 +437,7 @@ vowelMap = {
     "uai":4.0,
     "iao":6.0,
     "ao":7.0,
-    
+
     "uan":10.0,
     "an":11.0,
     "ang":12.0,
@@ -445,31 +445,32 @@ vowelMap = {
     "iang":15.0,
     "uang":17.0,
     "ua":18.0,
-    
+
     "o":21.0,
     "io":22.0,
     "ou":23.0,
+    "uo":24.0,
     "ong":26.0,
     "iong":27.0,
-    
+
     "e":31.0,
     "ei":33.0,
     "ie":34.0,
     "er":37.0,
-    
+
     "ve":40.0,
     "ue":40.0,
     "u:e":40.0,
-    
+
     "en":43.0,
     "eng":44.0,
-    
+
     "uen":45.0,
     "ueng":45.0,
-    
+
     "u:en":42.0,
     "ven":42.0,
-    
+
     "i":50.0,
     "u:":51.0,
     "v":51.0,
@@ -477,17 +478,17 @@ vowelMap = {
     "vn":53.0,
     "u:an":55.0,
     "v:an":55.0,
-    
+
     "in":53.0,
     "ing":55.0,
-    
+
     "u":60.0,
     "ui":63.0,
     "uei":63.0,
     "iu":64.0,
     "iou":64.0,
     "un":66.0,
-    
+
     "":99999.0,
     "__v":99999.0
 }
@@ -505,7 +506,7 @@ def getClosePinyinCandids(word, theta=2):
     res = []
     word_pinyin = to_pinyin(word)
     word_py = Pinyin(word_pinyin[0])
-    
+
     cCandids = getConsonantCandids(theta, word_py)
     for i in range(len(cCandids)):
         if cCandids[i] == word_py.consonant:
@@ -513,7 +514,7 @@ def getClosePinyinCandids(word, theta=2):
         for j in range(1,5,1):
             newPy = cCandids[i]+word_py.vowel+str(j)
             res.append(Pinyin(newPy))
-    
+
     vCandids = getVowelCandids(theta, word_py)
     for i in range(len(vCandids)):
         for j in range(1,5,1):
@@ -524,13 +525,13 @@ def getClosePinyinCandids(word, theta=2):
                 newPy = word_py.consonant+vCandids[i]+str(j)
             res.append(Pinyin(newPy))
     return res
-            
-    
-    
+
+
+
 def getConsonantCandids(theta, word_py):
     populateDoubleConsonantsMap()
     res = []
-    curCode = 0        
+    curCode = 0
     if word_py.consonant is None:
         orgCode = consonantMap["__v"]
     else:
@@ -541,12 +542,12 @@ def getConsonantCandids(theta, word_py):
                 if cand is not None:
                     res += cand
     return res
-    
+
 
 def getVowelCandids(theta, word_py):
     populateDoubleVowelsMap()
     res = []
-    curCode = 0        
+    curCode = 0
     orgCode = vowelMap[word_py.vowel]
     for i in range(int(orgCode-theta), int(orgCode+theta), 1):
         if float(i) in doubleVowelsMap:
@@ -562,9 +563,9 @@ def populateDoubleConsonantsMap():
     for consonant in hmCdouble:
         if hmCdouble[consonant] not in doubleConsonantsMap:
             doubleConsonantsMap[hmCdouble[consonant]] = []
-            
+
         doubleConsonantsMap[hmCdouble[consonant]].append(consonant)
-        
+
 def populateDoubleVowelsMap():
     if len(doubleVowelsMap) is not 0:
         return
@@ -572,9 +573,9 @@ def populateDoubleVowelsMap():
     for vowel in hmVdouble:
         if hmVdouble[vowel] not in doubleVowelsMap:
             doubleVowelsMap[hmVdouble[vowel]] = []
-            
-        doubleVowelsMap[hmVdouble[vowel]].append(vowel)        
-        
+
+        doubleVowelsMap[hmVdouble[vowel]].append(vowel)
+
 
 def getCandidates(sentence, mode="simplified", theta=1):
     candidates = []
@@ -596,12 +597,3 @@ def getCandidates(sentence, mode="simplified", theta=1):
             if searchKey.strip() in pinyin_to_traditional:
                 candidates+=pinyin_to_traditional[searchKey.strip()]
     return candidates
-
-
-
-
-
-
-
-
-
